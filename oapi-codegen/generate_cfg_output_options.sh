e239188af8a9f5b887171994fa432ffd6b7c0dcf
E.O. Stinson <yostinso@gmail.com>
2025-10-13 02:08:18 +0000

    Pull back in DHCP config

--- oapi-codegen/generate_cfg_output_options.sh
+++ oapi-codegen/generate_cfg_output_options.sh
@@ -2,6 +2,12 @@
 
 GOOD_TAGS="AUTH FIREWALL INTERFACE SYSTEM SERVICES"
 
+echo "# yaml-language-server: $schema=https://raw.githubusercontent.com/oapi-codegen/oapi-codegen/HEAD/configuration-schema.json"
+echo "package: pfsense_rest_v2"
+echo "output: ../internal/api/pfsense_rest_v2.gen.go"
+echo "generate:"
+echo "  models: true"
+echo "  client: true"
 echo "output-options:"
 echo "  include-tags:"
 for tag in $GOOD_TAGS; do
@@ -12,6 +18,7 @@ done
 before='["^/api/v2/services/dhcp_server$", "^/api/v2/services/dhcp_server/.*$" ] as $keep'
 from_tag="SERVICES"
 
+echo "  exclude-operation-ids:"
 jq -r "$before"' |
     .paths | with_entries(select(
         .value | with_entries(select(
@@ -22,5 +29,7 @@ jq -r "$before"' |
         .key as $key |
         any($keep[]; . as $re | $key | test($re)) | not
     ))
-    | keys' \
+    | to_entries | map(.value | to_entries | map(.value.operationId)) | flatten
+    | map("    - " + .) | join("\n")
+    ' \
     $(dirname $0)/openapi.json
